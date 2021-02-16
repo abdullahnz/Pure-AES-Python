a9bbd91f6dcbbda457d6e2186b24b120dad3c971
abdullahnz <edfallennzm@gmail.com>
2021-02-16 11:50:23 +0700

    Remove unused functions

--- main.py
+++ main.py
@@ -339,28 +339,11 @@ if __name__ == '__main__':
 
     print(f'ECB_MODE : {ecb.hex()}')
     print(f'DECRYPTED: {InverseECB(ecb, KEY)}\n')
-
     print(f'CFB_MODE : {cfb.hex()}')
     print(f'DECRYPTED: {InverseCFB(cfb, KEY, IV)}\n')
-
     print(f'CBC_MODE : {cbc.hex()}')
     print(f'DECRYPTED: {InverseCBC(cbc, KEY, IV)}\n')
-
     print(f'OFB_MODE : {ofb.hex()}')
     print(f'DECRYPTED: {InverseOFB(ofb, KEY, IV)}\n')
-
     print(f'PCBC_MODE: {pcbc.hex()}')
     print(f'DECRYPTED: {InversePCBC(pcbc, KEY, IV)}\n')
-
-
-    
-    
-    
-
-
-    
-    
-    
-    
-
-    
